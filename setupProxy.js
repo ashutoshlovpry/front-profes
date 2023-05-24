ac3196c70ae3ea3b6600710b09ad74391302570f
ashutosh <ashutoshsingh@clavax.com>
2023-05-24 11:08:44 +0530

    pus

--- setupProxy.js
+++ setupProxy.js
@@ -1,7 +1,9 @@
+import { BACKEND_HOST } from './src/constant';
+
 const  {createProxyMiddleware}  = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
-    '/',
+    BACKEND_HOST+'/login',
     createProxyMiddleware({
       target: 'https://new-prof.onrender.com',
       changeOrigin: true,
