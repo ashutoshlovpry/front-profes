8c2c2b53bf88c4cd2f390b10b4c4ad3ef4328b15
ashutosh <ashutoshsingh@clavax.com>
2023-06-14 10:55:16 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import Home from './component/Home';
-import './App.css';
-import {  Routes, Route, } from 'react-router-dom';
+//import './App.css';
+import {  Routes, Route,BrowserRouter } from 'react-router-dom';
 import Login from './component/login';
 import Chat from './component/chat';
+import Profile from './component/profile';
+import './style/app.scss'
+import React, { Component }  from 'react';
+
 function App() {
   return (
     <>
+    <BrowserRouter>
     <Routes>
 
 
@@ -14,8 +19,10 @@ function App() {
       <Route path='login' element={<Login/>}></Route>
       <Route path='/' element={<Home/>}></Route>
       <Route path='/chat' element={<Chat/>}></Route>
+      <Route path='/profile' element={<Profile/>}></Route>
+
       </Routes>
-   
+      </BrowserRouter>
     </>
         
   );
