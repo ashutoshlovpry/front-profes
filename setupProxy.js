96fa8c023b4d3ccd89bedf44fa21a2eea9f988ba
ashutoshlovpry <67589741+ashutoshlovpry@users.noreply.github.com>
2023-06-20 10:29:16 +0530

    proxy main

---
+++ setupProxy.js
@@ -0,0 +1,26 @@
+import { BACKEND_HOST } from './constant';
+
+const  {createProxyMiddleware}  = require('http-proxy-middleware');
+// module.exports = function(app) {
+//   app.use(
+    
+//     createProxyMiddleware('/',{
+//       target: 'https://new-prof.onrender.com',
+//       changeOrigin: true,
+//       pathRewrite: {
+//         '^/': '', // Remove the leading forward slash from the request path
+//       },
+//     })
+//   );
+//   console.log("proxy calling");
+// };
+
+module.exports = function(app) {
+  app.use(
+    '/',
+    createProxyMiddleware({
+      target: 'https://new-prof.onrender.com/',
+      changeOrigin: true,
+    })
+  );
+};
